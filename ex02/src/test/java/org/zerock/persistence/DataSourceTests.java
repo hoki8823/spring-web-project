efcc6aa454a4e0749432765fbaa2238a01e012ee
hoki8823 <hoki8823@naver.com>
2021-09-18 18:40:29 +0900

    스프링 MVC프로젝트의 기본구성

---
+++ ex02/src/test/java/org/zerock/persistence/DataSourceTests.java
@@ -0,0 +1,51 @@
+package org.zerock.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class DataSourceTests {
+	@Setter(onMethod = @__({@Autowired }))
+	private DataSource datasource;
+	@Setter(onMethod = @__({@Autowired }))
+	private SqlSessionFactory sqlSeesionFactory;
+	
+	@Test
+	public void testConnection() {
+		try (Connection conn = datasource.getConnection()){
+			log.info(conn);
+		} catch (Exception e) {
+			// TODO: handle exception
+			fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testMybatis() {
+		try (SqlSession session = sqlSeesionFactory.openSession();
+				Connection conn = session.getConnection();){
+			log.info(session);
+			log.info(conn);
+		} catch (Exception e) {
+			// TODO: handle exception
+			fail(e.getMessage());
+		}
+	}
+
+}
