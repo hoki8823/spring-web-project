4039614a14e213048af58801747cd438f40a685d
hoki8823 <hoki8823@naver.com>
2021-09-19 16:55:41 +0900

    비지니스 계층

---
+++ ex02/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,52 @@
+package org.zerock.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.log4j.Log4j;
+
+@Service
+@RequiredArgsConstructor
+@ToString
+public class BoardServiceImpl implements BoardService{
+
+	private final BoardMapper mapper;
+
+	@Override
+	public void register(BoardVO board) {
+		// TODO Auto-generated method stub
+		mapper.insertSelectKey(board);
+		
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		// TODO Auto-generated method stub
+		return mapper.read(bno);
+	}
+
+	@Override
+	public boolean modify(BoardVO board) {
+		// TODO Auto-generated method stub
+		return mapper.update(board) == 1;
+	}
+
+	@Override
+	public boolean remove(Long bno) {
+		// TODO Auto-generated method stub
+		return mapper.delete(bno) == 1;
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		// TODO Auto-generated method stub
+		return mapper.getList();
+	}
+}
