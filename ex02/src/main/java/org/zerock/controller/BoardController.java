867728c3396a7e5fd9381242b26ce33c4418a481
hoki8823 <hoki8823@naver.com>
2021-09-20 23:29:00 +0900

    프레제네이션(웹)계층의 CRUD구현

---
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,55 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
+import org.zerock.service.BoardService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/board/*")
+@RequiredArgsConstructor
+public class BoardController {
+	
+	private final BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		model.addAttribute("list",service.getList());
+	}
+	
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+
+		service.register(board);
+		rttr.addAttribute("result",board.getBno());
+		
+		return "redirect:/board/list";
+	}
+	
+	@PostMapping("/modify")
+	public String modify(BoardVO board,RedirectAttributes rttr) {
+
+		if(service.modify(board)) {
+			rttr.addFlashAttribute("result","success");
+		}
+		return "redirect:/board/list";
+	}
+	
+	
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno,RedirectAttributes rttr) {
+		
+		if(service.remove(bno)) {
+			rttr.addFlashAttribute("result","success");
+		}
+		return "redirect:/board/list";
+	}
+}
